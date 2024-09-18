aba1319f6eb6b547bf1e6473117bafc3269123aa
Lennart <wiebusch.lennart@gmail.com>
2024-09-18 19:25:22 +0200

    Create new Java Fabric Project

---
+++ src/main/java/de/lenno/citygenerator/CityGeneratorClientClass.java
@@ -0,0 +1,11 @@
+package de.lenno.citygenerator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CityGeneratorClientClass implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
